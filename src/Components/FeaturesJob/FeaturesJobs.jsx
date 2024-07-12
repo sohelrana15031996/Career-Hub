1fc5091bcfa9d714aa6c20621b96e4bf44ad75f8
Sohel Rana <sohelrana130523@gmail.com>
2024-07-12 23:23:47 +0100

    Data loaded from json file

--- src/Components/FeaturesJob/FeaturesJobs.jsx
+++ src/Components/FeaturesJob/FeaturesJobs.jsx
@@ -1,11 +1,26 @@
+import { useContext, useState } from "react";
+import { JobContext } from "../Home/Home";
+import JobList from "../JobList/JobList";
 
 
 const FeaturesJobs = () => {
+  const jobs = useContext(JobContext);
+  console.log(jobs);
+  const [count, setCount]= useState(4);
+  const addAll = () => setCount(count+(jobs.length - 1));
   return (
     <div className="">
       <div className="text-center">
         <h1 className="text-5xl">Featured Jobs</h1>
         <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+      </div>
+      <div>
+        {
+          jobs.slice(0,count).map(job => <JobList key={job.job_id} job={job}></JobList>)
+        }
+      </div>
+      <div>
+        <button className="" onClick={addAll}>Show more</button>
       </div>
     </div>
   );
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,31 @@
+import { createContext, useEffect, useState } from "react";
 import Banner from "../Banner/Banner";
 import FeaturesJobs from "../FeaturesJob/FeaturesJobs";
 import JobCatagory from "../JobCatagory/JobCatagory";
 
 
-
+export const JobContext = createContext(1);
 const Home = () => {
+  const [liveJob, setLiveJob] = useState([]);
+  useEffect(() => {
+    fetch('/Data.json')
+      .then(res => res.json())
+      .then(data => setLiveJob(data))
+  }, []);
 
-  
+  // Creating context api
+
+
+
+
   return (
     <div>
       <div>
-        <Banner></Banner>
-        <JobCatagory></JobCatagory>
-        <FeaturesJobs></FeaturesJobs>
+        <JobContext.Provider value={liveJob}>
+          <Banner></Banner>
+          <JobCatagory></JobCatagory>
+          <FeaturesJobs></FeaturesJobs>
+        </JobContext.Provider>
       </div>
     </div>
   );
